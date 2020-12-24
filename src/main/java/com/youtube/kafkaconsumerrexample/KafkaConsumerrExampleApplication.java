cb8e135075c024f8e41371d7f40efb668a5f6cff
vinod developer <vinod.allam74@gmail.com>
2020-12-24 22:42:40 +0530

    kafka consumer example

---
+++ src/main/java/com/youtube/kafkaconsumerrexample/KafkaConsumerrExampleApplication.java
@@ -0,0 +1,14 @@
+package com.youtube.kafkaconsumerrexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerrExampleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KafkaConsumerrExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/listener/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package listener;
+
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+
+    @KafkaListener(topics ="december" , groupId = "group_id")
+    public void consume(String message){
+        System.out.println("consumed message  "+message);
+    }
+}
